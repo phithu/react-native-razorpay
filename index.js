c17292fd377ce83ce581b175a87df6ea995e33f7
Phi Thu <phithu.dev@gmail.com>
2020-07-03 14:41:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-'use strict';
+"use strict";
 
-import { NativeModules, NativeEventEmitter } from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  Platform,
+} from "react-native";
 
-
-const razorpayEvents = new NativeEventEmitter(NativeModules.RazorpayEventEmitter);
+const razorpayEvents = Platform.OS === "android" ? new NativeEventEmitter(
+  NativeModules.RazorpayEventEmitter) : null;
 
 const removeSubscriptions = () => {
-  razorpayEvents.removeAllListeners('Razorpay::PAYMENT_SUCCESS');
-  razorpayEvents.removeAllListeners('Razorpay::PAYMENT_ERROR');
-  razorpayEvents.removeAllListeners('Razorpay::EXTERNAL_WALLET_SELECTED');
+  if (Platform.OS === "android") {
+    razorpayEvents.removeAllListeners("Razorpay::PAYMENT_SUCCESS");
+    razorpayEvents.removeAllListeners("Razorpay::PAYMENT_ERROR");
+    razorpayEvents.removeAllListeners("Razorpay::EXTERNAL_WALLET_SELECTED");
+  }
 };
 
 class RazorpayCheckout {
   static open(options, successCallback, errorCallback) {
     return new Promise(function(resolve, reject) {
-      razorpayEvents.addListener('Razorpay::PAYMENT_SUCCESS', (data) => {
+      razorpayEvents.addListener("Razorpay::PAYMENT_SUCCESS", (data) => {
         let resolveFn = successCallback || resolve;
         resolveFn(data);
         removeSubscriptions();
       });
-      razorpayEvents.addListener('Razorpay::PAYMENT_ERROR', (data) => {
+      razorpayEvents.addListener("Razorpay::PAYMENT_ERROR", (data) => {
         let rejectFn = errorCallback || reject;
         rejectFn(data);
         removeSubscriptions();
@@ -27,8 +33,9 @@ class RazorpayCheckout {
       NativeModules.RazorpayCheckout.open(options);
     });
   }
+
   static onExternalWalletSelection(externalWalletCallback) {
-    razorpayEvents.addListener('Razorpay::EXTERNAL_WALLET_SELECTED', (data) => {
+    razorpayEvents.addListener("Razorpay::EXTERNAL_WALLET_SELECTED", (data) => {
       externalWalletCallback(data);
       removeSubscriptions();
     });
